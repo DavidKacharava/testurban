883be7657dfa5e1430624fb5830d11edbbad07bf
DavidKacharava <kdato911@gmail.com>
2024-06-13 11:37:05 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите ваше имя и фамилию:')
+print(my_string. upper())
+print(my_string. lower())
+print(my_string.replace(" ", ""))
+first_character = my_string[0]
+print(first_character)
+last_character = my_string[-1]
+print(last_character)
